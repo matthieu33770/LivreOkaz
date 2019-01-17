913de5b83e179ecc7950db1d0680a20659168b51
SIMFOR078 <matthielondeix@gmail.com>
2019-01-17 09:37:45 +0100

    Préparation Security

---
+++ src/main/java/filRouge/BCrypt.java
@@ -0,0 +1,10 @@
+package filRouge;
+
+public class BCrypt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/filRouge/InMemoryWebSecurity.java
@@ -0,0 +1,5 @@
+package filRouge;
+
+public class InMemoryWebSecurity {
+
+}
