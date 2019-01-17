1e42971b2bca690dfbd6960d91ad862d5828b521
SamuelJOBLON <samuel.joblon@gmail.com>
2019-01-17 13:59:12 +0100

    buildgradle

--- src/main/java/filRouge/controller/CommandeController.java
+++ src/main/java/filRouge/controller/CommandeController.java
@@ -1,5 +1,153 @@
 package filRouge.controller;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import filRouge.model.Commande;
+import filRouge.model.Users;
+import filRouge.repository.CommandeRepository;
+
+
+@RestController
+@RequestMapping("/api")
 public class CommandeController {
 
+	@Autowired
+	private CommandeRepository commandeRepository;
+	
+	/**
+	 * Get all the commandes.
+	 * @return a list with all the commandes
+	 * @throws Exception 
+	 */
+	@RequestMapping(value = "/commandes", method = RequestMethod.GET)
+	public ResponseEntity<List<Commande>> getAllCommandes(){
+		System.out.println(">> ok findAll");
+		List<Commande> listCommande = null;
+		try {
+			listCommande = commandeRepository.findAll();
+			for (Commande commande : listCommande) {
+				System.out.println(commande);
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(listCommande);
+	}
+	
+	/**
+	 * Get a specific commande based on ID
+	 * @param id : the id of commande.
+	 * @return the commande
+	 * @throws Exception 
+	 */
+	@RequestMapping(value = "/commande", method = RequestMethod.GET)
+	public ResponseEntity<?> getCommande(@PathVariable Integer idCommande) {		
+		System.out.println(">> ok findOne");
+		
+		Optional<Commande> commande = null;
+		
+		try {
+			commande = commandeRepository.findById(idCommande);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+		
+		if(commande == null)
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(commande);
+	}
+	
+	/**
+	 * Create a new commande.
+	 * @param commande : the commande information.
+	 * @throws Exception 
+	 */
+	@RequestMapping(value="/commande", method = RequestMethod.POST)
+	public ResponseEntity<?> addCommande(@RequestBody Commande commande){
+		Commande resultCommande = null;
+		
+		Date date = commande.getDate();
+		Double prixHT = commande.getPrixHT();
+		Double prixTTC = commande.getPrixTTC();
+		Double tva = commande.getTva();
+		Double fraisDePort = commande.getFraisDePort();
+		Double total = commande.getTotal();
+		Users idClient = commande.getIdClient();
+		Users nomClient = commande.getNomClient();
+		Users prenomClient = commande.getPrenomClient();
+		Users numeroFacturation = commande.getNumeroFacturation();
+		Users rueFacturation = commande.getRueFacturation();
+		Users complementFacturation = commande.getComplementFacturation();
+		Users cpFacturation = commande.getCpFacturation();
+		Users villeFacturation = commande.getVilleFacturation();
+		Users numeroLivraison = commande.getNumeroLivraison();
+		Users rueLivraison = commande.getRueLivraison();
+		Users complementLivraison = commande.getComplementLivraison();
+		Users cpLivraison = commande.getCpLivraison();
+		Users villeLivraison = commande.getVilleLivraison();
+		
+		System.out.println(">> "+commande);
+		
+		if(date == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The date is not set !");
+		if(prixHT == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The prixHT is not set !");
+		if(prixTTC == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The prixTTC is not set !");
+		if(tva == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The tva is not set !");
+		if(fraisDePort == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The fraisDePort is not set !");
+		if(total == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The total is not set !");
+		if(idClient == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The idClient is not set !");
+		if(nomClient == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The nomClient is not set !");		
+		if(prenomClient == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The prenomClient is not set !");
+		if(numeroFacturation == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The numeroFacturation is not set !");
+		if(rueFacturation == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The rueFacturation is not set !");
+		if(complementFacturation == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The complementFacturation is not set !");
+		if(cpFacturation == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The cpFacturation is not set !");
+		if(villeFacturation == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The villeFacturation is not set !");
+		if(numeroLivraison == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The numeroLivraison is not set !");
+		if(rueLivraison == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The rueLivraison is not set !");
+		if(complementLivraison == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The complementLivraison is not set !");
+		if(cpLivraison == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The cpLivraison is not set !");
+		if(villeLivraison == null)
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The villeLivraison is not set !");
+		
+		try {
+			resultCommande = commandeRepository.saveAndFlush(commande);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(resultCommande);
+
+	}	
+	
 }
