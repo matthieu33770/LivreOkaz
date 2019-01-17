a843bb117a90ab93aa0e9d42a1a3adfb25e3508f
SIMFOR078 <matthielondeix@gmail.com>
2019-01-17 16:20:43 +0100

    Controller Role

--- src/main/java/filRouge/controller/RoleController.java
+++ src/main/java/filRouge/controller/RoleController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import filRouge.model.Role;
@@ -119,7 +118,7 @@ public class RoleController {
 	 * @throws Exception
 	 */
 	@DeleteMapping("/user/{id}")
-	public @ResponseBody void deleteRole(@PathVariable Integer id){
+	public ResponseEntity<?> deleteRole(@PathVariable Integer id){
 		try {
 		roleRepository.deleteById(id);
 		} catch (Exception e) {
